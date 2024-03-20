b5000e0d4dd9a7cbf8bf3a278dbc3e33e25d502d
Jennika Elisson <jennika.elisson@gmail.com>
2024-03-20 15:59:18 +0100

    update main.tsx

--- client/image-app-react/src/main.tsx
+++ client/image-app-react/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Router'
+import { Auth0Provider } from '@auth0/auth0-react';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-jowmadvmnatj57rz.eu.auth0.com"
+      clientId="GqYTAKDKk3IgVOHZ2fdJsH6pMyYoRrg9"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >    <RouterProvider router={router}></RouterProvider>
+
+      
+    </Auth0Provider>
+  </React.StrictMode>
+);
