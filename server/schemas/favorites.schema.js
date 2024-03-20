a3385c753be26c5d17a30310d3515c5c28c7fc7c
Jennika Elisson <jennika.elisson@gmail.com>
2024-03-20 15:45:25 +0100

    added schema

---
+++ server/schemas/favorites.schema.js
@@ -0,0 +1,9 @@
+const Joi = require("joi")
+
+const saveSchema = Joi.object({
+    userEmail: Joi.string().required(),
+    imageUrl: Joi.string().required(),
+    imageTitle: Joi.string().required()
+})
+
+module.exports = { saveSchema }
